842624c5d9f4582d0f4242344e851260eb19b2b0
zhupanovdm <zhupanov.dmitry@gmail.com>
2021-12-28 12:40:13 +0300

    Revert "Sprint 01 Increment 02"

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,40 +1,15 @@
 package main
 
 import (
-	"context"
-	"flag"
-	"fmt"
-	"github.com/zhupanovdm/go-runtime-monitor/cmd/server/handlers"
-	"github.com/zhupanovdm/go-runtime-monitor/internal/app"
 	"log"
 	"net/http"
 )
 
-var port = flag.Int("p", 8080, "Server port")
-
 func main() {
-	app.Once(server(*port, handlers.NewRouter()))
-	app.Serve()
+	http.Handle("/", http.HandlerFunc(metricUpdateHandler))
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
 
-func server(port int, handler http.Handler) app.ExecutorHandler {
-	server := &http.Server{
-		Addr:    fmt.Sprintf("localhost:%d", port),
-		Handler: handler,
-	}
-
-	return app.ExecutorHandler{
-		OnStart: func() {
-			log.Printf("starting server at %v", server.Addr)
-		},
-		OnExec: func(ctx context.Context, cancel context.CancelFunc) {
-			log.Fatal(server.ListenAndServe())
-		},
-		OnEnd: func() {
-			log.Println("closing server")
-			if err := server.Close(); err != nil {
-				log.Fatal(err)
-			}
-		},
-	}
+func metricUpdateHandler(_ http.ResponseWriter, req *http.Request) {
+	log.Printf("%s %s\n", req.Method, req.URL.Path)
 }
